717189cf0b0bfe6f535b43284511f11b9fad056f
Kusamthakur <kusamthakur433@gmail.com>
2025-05-07 21:43:05 +0530

    Initial commit

---
+++ src/Cube/Cube.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Cube.css'
+
+const Cube = () => {
+  return (
+    <div className='body'>
+      
+      <div className="cube-container">
+        <div className="cube">
+            <div className="faces"></div>
+            <div className="faces"></div>
+            <div className="faces"></div>
+            <div className="faces"></div>
+            <div className="faces"></div>
+            <div className="faces"></div>
+            <div className="faces"></div>
+            <div className="faces"></div>
+         
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cube
